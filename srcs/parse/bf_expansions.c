c79380b36007ba8ab0a502bf3780aeee954098c6
vax <vax@debian.vaxivity>
2021-12-17 16:21:04 +0100

    babelfish(token, expansions)

--- srcs/parse/bf_expansions.c
+++ srcs/parse/bf_expansions.c
@@ -12,42 +12,81 @@
 
 #include "../../includes/parse.h";
 
-void	exp_try(char *temp, char *expand, t_ms data)
+int		iseov(char *c)
 {
+	if (c < 48)
+		return (0);
+	else if (c >= 65 && c <= 90)
+		return (0)
+	else if (c >= 97 && c <= 122)
+		return (0);
+	else if (c == '_')
+		return (0);
+	return (1);
+}
+
+void exp_insert(char *str, char *mod, char *res)
+{
+	int		i;
+	char	*start;
+	char	*end;
+
+	i = 0;
+	while (str[i] != '$' /*&& !quoted_exp(str, i)*/)
+		i++;
+	start = ft_strjoin(ft_substr(str, 0, i), res);
+	end = ft_strdup(ft_substr(str, i, ft_strlen(str) - i));
+	str = ft_strjoin(start, end);
+	free(start);
+	free(end);
 	return ;
 }
 
-int		exp_change(char *expand, char *str, t_ms data)
+void	exp_mod(char *expand, char *str, char **env)
 {
-	return (0);
+	int i;
+	char	*mod;
+	char	*temp;
+
+	i = 0;
+	mod = ft_strdup("");
+	temp = ft_strjoin(expand, "=");
+	while (env[i])
+	{
+		if (ft_strncmp(temp, env[i], ft_strlen(temp)) == 0)
+			mod = ft_strdup(ft_substr(env[i], ft_strlen(temp), ft_strlen(env[i]) - ft_strlen(temp)));
+		i++;
+	}
+	free(temp);
+	exp_insert(str, expand, mod);
+	free(mod);
+	return ;
 }
 
 void	bf_expansions(char *str, t_ms data)
 {
 	int		i;
-	char	*temp;
+	int		control;
 	char	*expand;
 
 	i = 0;
-	temp = ft_strdup("");
-	expand = ft_strdup("");
+	control = -1;
 	while (str[i] != 0)
 	{
-		if (str[i] == '$' /*&& !quoted_exp(str, i)*/)
+		if (iseov(str[i]) && control != -1)
 		{
-			if (ft_strlen(temp) > 0)
-				break;
-			temp = ft_strdup("$");
+			expand = ft_strdup(ft_substr(str, control, i - control));
+			break;
 		}
+		else if (str[i] == '$' /*&& !quoted_exp(str, i)*/)
+			control = i + 1;
 		i++;
 	}
-	//exp_try(temp, expand, data);			//on working...
-	if (ft_strlen(expand) > 0)
+	if (control != -1)
 	{
-		//exp_change(expand, str, data);			//on working...
+		exp_mod(expand, str, data);
+		free(expand);
 		bf_expansions(str, env);
 	}
-	free(temp);
-	free(expand);
-	return  ;
+	return ;
 }
