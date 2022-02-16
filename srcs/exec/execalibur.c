3deed1ba85debdd9cab16f865de9bab23b3943fe
Oscar Mercade de lucas <omercade@c1r1s2.42madrid.com>
2022-02-16 20:02:38 +0100

    exec: pipes working

--- srcs/exec/execalibur.c
+++ srcs/exec/execalibur.c
@@ -6,130 +6,41 @@
 /*   By: omercade <omercade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 18:07:23 by omercade          #+#    #+#             */
-/*   Updated: 2022/02/15 20:19:53 by omercade         ###   ########.fr       */
+/*   Updated: 2022/02/16 19:56:23 by omercade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-//------>FUNCIONAL<------//
-// void	execalibur(t_ms *this)
-// {
-// 	t_list	*aux;
-
-// 	aux = this->tokenst;
-
-// 	//==1ST PROCESS==//
-// 	printf("FIRST PROCESS\n");
-// 	if (aux->next)
-// 	{
-// 		pipe(((t_token *)(aux->next->content))->fd);
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		//Check fork error!!
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			dup2(((t_token *)(aux->next->content))->fd[1], STDOUT_FILENO);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_OPEN_PROCESS
-// 	}
-// 	else
-// 	{
-// 		((t_token *)(aux->content))->pid = fork();
-// 		if (!((t_token *)(aux->content))->pid)
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		else
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 	}
-// 	aux = aux->next;
-
-// 	//==MULTI-PROCESS==//
-// 	int i = 1;			/*Depuracion*/
-// 	while (aux && aux->next)
-// 	{
-// 		printf("MULTI-PROCESS #%d\n", i);
-// 		pipe(((t_token *)(aux->next->content))->fd);
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			dup2(((t_token *)(aux->next->content))->fd[1], STDOUT_FILENO);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			dup2(((t_token *)(aux->content))->fd[0], STDIN_FILENO);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_MULTI-PROCESS
-// 		aux = aux->next;
-// 		i++;			/*Depuracion*/
-// 	}
-
-// 	//==LAST PROCESS==//
-// 	if (aux)
-// 	{
-// 		printf("CLOSE PROCESS\n");
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		//Check fork error!!
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			dup2(((t_token *)(aux->content))->fd[0], STDIN_FILENO);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_CLOSE_PROCESS
-// 	}
-// }
-
-
-//------>TEST<------//
-void	close_process(t_list *token, char **env)
+void	close_process(t_list *token, char **env, t_list *tokenst)
 {
-	if (!token)
-		printf("A!\n");
+	t_list	*aux;
+
+	aux = tokenst;
+
 	((t_token *)(token->content))->pid = fork();
 	if (!((t_token *)(token->content))->pid)
 	{
-		close(((t_token *)(token->content))->fd[1]);
 		dup2(((t_token *)(token->content))->fd[0], STDIN_FILENO);
 		close(((t_token *)(token->content))->fd[0]);
+		close(((t_token *)(token->content))->fd[1]);
 		exe_process((t_token *)(token->content), env);
 	}
 	else
 	{
-		close(((t_token *)(token->content))->fd[1]);
 		close(((t_token *)(token->content))->fd[0]);
-		//waitpid(((t_token *)(token->content))->pid, NULL, 0);
+		close(((t_token *)(token->content))->fd[1]);
+	}
+
+	while (aux)
+	{
+		int status;
+		waitpid(((t_token *)(aux->content))->pid, &status, 0);
+		aux = aux->next;
 	}
 }
 
-void	multi_process(t_list *token, char **env)
+void	multi_process(t_list *token, char **env, t_list *tokenst)
 {
 	if (token->next)
 	{
@@ -138,28 +49,25 @@ void	multi_process(t_list *token, char **env)
 		if (!((t_token *)(token->content))->pid)
 		{
 			close(((t_token *)(token->content))->fd[1]);
-			close(((t_token *)(token->next->content))->fd[0]);
-			dup2(((t_token *)(token->next->content))->fd[1], STDOUT_FILENO);
-			close(((t_token *)(token->next->content))->fd[1]);
 			dup2(((t_token *)(token->content))->fd[0], STDIN_FILENO);
 			close(((t_token *)(token->content))->fd[0]);
+			dup2(((t_token *)(token->next->content))->fd[1], STDOUT_FILENO);
+			close(((t_token *)(token->next->content))->fd[1]);
+			close(((t_token *)(token->next->content))->fd[0]);
 			exe_process((t_token *)(token->content), env);
 		}
 		else
 		{
-			close(((t_token *)(token->content))->fd[1]);
-			close(((t_token *)(token->next->content))->fd[0]);
-			close(((t_token *)(token->next->content))->fd[1]);
 			close(((t_token *)(token->content))->fd[0]);
-			multi_process(token->next, env);
-			//waitpid(((t_token *)(token->content))->pid, NULL, 0);
+			close(((t_token *)(token->content))->fd[1]);
+			multi_process(token->next, env, tokenst);
 		}
 	}
 	else
-		close_process(token, env);
+		close_process(token, env, tokenst);
 }
 
-void	open_process(t_list *token, char **env)
+void	open_process(t_list *token, char **env, t_list *tokenst)
 {
 	pipe(((t_token *)(token->next->content))->fd);
 	((t_token *)(token->content))->pid = fork();
@@ -171,17 +79,10 @@ void	open_process(t_list *token, char **env)
 		exe_process((t_token *)(token->content), env);
 	}
 	else
-	{
-		close(((t_token *)(token->next->content))->fd[0]);
-		close(((t_token *)(token->next->content))->fd[1]);
-		multi_process(token->next, env);
-		while (token)
-		{
-			waitpid(((t_token *)(token->content))->pid, NULL, 0);
-			token = token->next;
-		}
-	}
+		multi_process(token->next, env, tokenst);
 }
+
+
 
 void	execalibur(t_ms *this)
 {
@@ -189,7 +90,7 @@ void	execalibur(t_ms *this)
 
 	aux = this->tokenst;
 	if (aux->next)
-		open_process(aux, this->env);
+		open_process(aux, this->env, this->tokenst);
 	else
 	{
 		((t_token *)(aux->content))->pid = fork();
@@ -200,96 +101,4 @@ void	execalibur(t_ms *this)
 	}
 }
 
-// void	execalibur(t_ms *this)
-// {
-// 	t_list	*aux;
 
-// 	aux = this->tokenst;
-
-// 	//==1ST PROCESS==//
-// 	printf("FIRST PROCESS\n");
-// 	if (aux->next)
-// 	{
-// 		pipe(((t_token *)(aux->next->content))->fd);
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		//Check fork error!!
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			dup2(((t_token *)(aux->next->content))->fd[1], STDOUT_FILENO);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_OPEN_PROCESS
-// 	}
-// 	else
-// 	{
-// 		((t_token *)(aux->content))->pid = fork();
-// 		if (!((t_token *)(aux->content))->pid)
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		else
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 	}
-// 	aux = aux->next;
-
-// 	//==MULTI-PROCESS==//
-// 	int i = 1;			/*Depuracion*/
-// 	while (aux && aux->next)
-// 	{
-// 		printf("MULTI-PROCESS #%d\n", i);
-// 		pipe(((t_token *)(aux->next->content))->fd);
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			dup2(((t_token *)(aux->next->content))->fd[1], STDOUT_FILENO);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			dup2(((t_token *)(aux->content))->fd[0], STDIN_FILENO);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->next->content))->fd[0]);
-// 			close(((t_token *)(aux->next->content))->fd[1]);
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_MULTI-PROCESS
-// 		aux = aux->next;
-// 		i++;			/*Depuracion*/
-// 	}
-
-// 	//==LAST PROCESS==//
-// 	if (aux)
-// 	{
-// 		printf("CLOSE PROCESS\n");
-// 		//	↑ Done!				↓ Not finished...
-// 		((t_token *)(aux->content))->pid = fork();
-// 		//Check fork error!!
-// 		if (!((t_token *)(aux->content))->pid)
-// 		{
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			dup2(((t_token *)(aux->content))->fd[0], STDIN_FILENO);
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			exe_process((t_token *)(aux->content), this->env);
-// 		}
-// 		else
-// 		{
-// 			close(((t_token *)(aux->content))->fd[0]);
-// 			close(((t_token *)(aux->content))->fd[1]);
-// 			waitpid(((t_token *)(aux->content))->pid, NULL, 0);
-// 		}
-// 		//	END EXE_CLOSE_PROCESS
-// 	}
-// }
