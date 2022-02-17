d3e6a7b86a211533224e1514cf26d85f34411362
Manuel Garcia sanchez <mangarci@c3r5s4.42madrid.com>
2022-02-17 19:05:24 +0100

    working on signals

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,11 +6,13 @@
 /*   By: adiaz-do <adiaz-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 20:23:32 by omercade          #+#    #+#             */
-/*   Updated: 2022/02/16 20:25:18 by adiaz-do         ###   ########.fr       */
+/*   Updated: 2022/02/17 19:04:09 by mangarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int		g_exit_status;
 
 int	main(int narg, char **xarg, char **env)
 {
@@ -24,10 +26,13 @@ int	main(int narg, char **xarg, char **env)
 	if (narg != 1)
 		return (!printf("ARGS ERROR!!\n"));
 	/*INITIALIZE*/
+	//g_exit_status = 0;
 	this.exit = 0;
 	this.in = dup(STDIN);
 	this.out = dup(STDOUT);
 	this.env = env;
+	//signal(SIGINT, signal_handler);
+	//signal(SIGQUIT, SIG_IGN);
 	//reset_fds
 	//set_env & set_secret_env
 	/*END_INIT*/
@@ -38,6 +43,8 @@ int	main(int narg, char **xarg, char **env)
 		if (ft_strlen(buf) != 0)
 		{
         	add_history(buf);
+			//signal(SIGINT, signal_handler);
+			//signal(SIGQUIT, SIG_IGN);
 			this.tokenst = babelfish(ft_strjoin(buf, "\0"), env);		//PARSER
 			execalibur(&this);
 		}
